09970f0f1f0f53f2b4ca27116e2d554948710e23
muahchee <187490843+muahchee@users.noreply.github.com>
2024-11-28 18:22:59 +1300

    Add score variables
    Also changed humanChoice and computerChoice to have string values,
    to differenciate them from the score value

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 // create variable called ComputerChoice
 
 // create function called getComputerChoice
-//     randomly generate number between 0 to 2 and store output in ComputerChoice
 
-//     If ComputerChoice is 0, print "Rock" into console
-//     Else if ComputerChoice is 1, print "Paper" into console
-//     Else print "Scissors" into console
+//     create a variable called randomChoiceNumber 
+//     generate number between 0 to 2 and store output in randomChoiceNumber
+
+//     If randomChoiceNumber is 0, store "Rock" in ComputerChoice and print "Rock" into console
+
+//     Else if ComputerChoice is 1, store "Paper" in ComputerChoice and print "Paper" into console
+
+//     Else store "Scissors" in ComputerChoice and print "Scissors" into console
 
 
 let computerChoice;
 
 function getComputerChoice(){
-    computerChoice = Math.floor(Math.random() * 3)
+    let randomChoiceNumber;
+    randomChoiceNumber = Math.floor(Math.random() * 3)
     
-    if (computerChoice === 0){
+    if (randomChoiceNumber === 0){
+        computerChoice = "Rock";
         console.log("Rock! The robot holds out a pretty round pebble from it's collection.")
-    }else if (computerChoice === 1){
+    }else if (randomChoiceNumber === 1){
+        computerChoice = "Paper";
         console.log("Paper! The robot prints an A4 sheet from it's mouth while looking you in the eyes.")
     }else{
+        computerChoice = "Scissors";
         console.log("Scissors! The robot shoves its C-shaped grippy hand at you.")
     }
 }
@@ -28,30 +36,25 @@ getComputerChoice();
 
 // Create function called getHumanChoice
 
-//     create variable humanInput
-//     prompt player to write their choice and store in humanInput
-//     In humanInput, capitalise the first letter and make the rest lower case
+//     prompt player to write their choice and store in humanChoice
+//     In humanChoice, capitalise the first letter and make the rest lower case
 
-//     If humanInput equals "Rock", store 0 in humanChoice and print "Rock" in console,
-//     Else if humanInput equals "Paper", store 1 in humanChoice and print "Paper" in console,
-//     Else if humanInput equals "Scissors", store 2 in humanChoice and print "Scissors" in console,
+//     If humanChoice equals "Rock", print "Rock" in console,
+//     Else if humanChoice equals "Paper", print "Paper" in console,
+//     Else if humanChoice equals "Scissors", print "Scissors" in console,
 //     Else print "That's not a valid choice! ...or maybe you spelt it wrong. Either way, please type Rock, Paper or Scissors." in console. Allow player to re-enter choice.
 
 let humanChoice;
 
 function getHumanChoice(){
-    let humanInput;
-    humanInput = prompt("Rock, Paper or Scissors?");
-    humanInput = humanInput.charAt(0).toUpperCase() + humanInput.slice(1).toLowerCase();
+    humanChoice = prompt("Rock, Paper or Scissors?");
+    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
 
-    if (humanInput === "Rock"){
-        humanChoice = 0;
+    if (humanChoice === "Rock"){
         console.log("You choose Rock!");
-    }else if(humanInput === "Paper"){
-        humanChoice = 1;
+    }else if(humanChoice === "Paper"){
         console.log("You choose Paper!");
-    }else if(humanInput === "Scissors"){
-        humanChoice = 2;
+    }else if(humanChoice === "Scissors"){
         console.log("You choose Scissors!")
     }else{
         console.log("That's not a valid choice! ...or maybe you spelt it wrong. Either way, please just type Rock, Paper or Scissors.")
@@ -61,3 +64,8 @@ function getHumanChoice(){
 
 getHumanChoice();
     
+// create a variable called humanScore and set it to 0
+// create a variable called computerScore and set it to 0
+
+let humanScore = 0;
+let computerScore = 0;
